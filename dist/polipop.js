b1592989e8a13ff55b200bc6e3978ce9df83fc73
Minitek <info@minitek.gr>
2021-05-01 18:47:32 +0300

    Installed eslint-config-standard

--- dist/polipop.js
+++ dist/polipop.js
@@ -24,7 +24,7 @@
  *  See the Polipop Commercial License at https://www.minitek.gr/licenses/polipop.
  */
  
-(function webpackUniversalModuleDefinition(root, factory) {
+(function webpackUniversalModuleDefinition(root, factory) 
     if (typeof exports === 'object' && typeof module === 'object')
         module.exports = factory();
     else if (typeof define === 'function' && define.amd) define([], factory);
